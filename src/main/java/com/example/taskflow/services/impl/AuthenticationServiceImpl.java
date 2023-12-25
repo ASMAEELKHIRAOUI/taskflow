55cb18a9eaec31ac3330f390b5edaa4f5ee7a647
ASMAEELKHIRAOUI <asmaaekw@gmail.com>
2023-12-25 11:26:20 +0100

    authentification working

---
+++ src/main/java/com/example/taskflow/services/impl/AuthenticationServiceImpl.java
@@ -0,0 +1,49 @@
+package com.example.taskflow.services.impl;
+
+import com.example.taskflow.config.JwtService;
+import com.example.taskflow.domain.User;
+import com.example.taskflow.domain.enums.Role;
+import com.example.taskflow.dto.requests.AuthenticationRequest;
+import com.example.taskflow.dto.requests.RegisterRequest;
+import com.example.taskflow.dto.responses.AuthenticationResponse;
+import com.example.taskflow.repositories.UserRepository;
+import com.example.taskflow.services.AuthenticationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationServiceImpl implements AuthenticationService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+    @Override
+    public AuthenticationResponse register(RegisterRequest request) {
+        var user = User.builder()
+                .firstName(request.getFirstName())
+                .lastName(request.getLastName())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+        userRepository.save(user);
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder().token(jwtToken).build();
+    }
+
+    @Override
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword())
+        );
+        var user = userRepository.findByEmail(request.getEmail()).orElseThrow();
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse.builder().token(jwtToken).build();
+    }
+}
